24e87d2f47d8b6f664e75fc86a4122f5a72dd370
HamidProg-22 <197288102+HamidProg-22@users.noreply.github.com>
2025-10-19 05:08:47 +0330

    create tables contact and update Post
    created Contact Table and update Post Table, designed schme of database into draw sql and updates models files in blog and website.

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-
+# Create table Post for blog page and for add a post to blog
 class Post(models.Model):
+    # image 
+    # author
     title = models.CharField(max_length=255)
     content = models.TextField()
+    # tag
+    # category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    
+                                                                
