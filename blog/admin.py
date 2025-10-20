88961898b04c86df92b184664fd8ee4be04a5002
HamidProg-22 <197288102+HamidProg-22@users.noreply.github.com>
2025-10-20 04:18:17 +0330

    Update admin.py
    Customize the admin panel- created class PostAdmin for admin file and update it.

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,17 @@ from django.contrib import admin
 from .models import Post   #  or from blog.models import Post
 # Register your models here.
 
-admin.site.register(Post)
+
+# @admin.register(Post) == admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'   # added item date for easy access
+    # empty_value_display = '-emptey-'  # for show empty values of tables
+    # fields = ('title', )
+    list_display = ('title', 'counted_views', 'status', 'published_date','created_date')
+    list_filter = ('status', )
+    ordering = ['-created_date']  # - is ordered by dec
+    search_fields = ['title']
+
+
+   
+admin.site.register(Post, PostAdmin)
