7d2999acc64ad3027066c9e8e5ea5d0c72819708
HamidProg-22 <197288102+HamidProg-22@users.noreply.github.com>
2025-10-22 08:47:13 +0330

    created sample file
    created a sample file for urls.py and views.py on blog

--- blog/urls.py
+++ blog/urls.py
@@ -8,13 +8,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', blog_view, name= 'index'),
-    # path('single', blog_single, name= 'single'),
-    # path('post-<int:pid>', blog_single, name= 'single'),
     path('<int:pid>', blog_single, name= 'single'),
-    # path('test', test, name='test'),
-    # path('<str:name>', test, name='test')
-    # path('<str:name>/<str:family_name>/<int:age>', test, name='test')
-    # path('<str:name>/lastname/<str:family_name>/age/<int:age>', test, name='test')
-    # path('post-<int:pid>', test, name='test')
+    path('post-<int:pid>', test, name='test')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-# from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
@@ -14,39 +13,8 @@ def blog_single(request, pid):
     context = {'post':post}
     return render(request, 'blog/blog-single.html', context)
 
-# def test(request):
-#     posts = Post.objects.all()
-#     # posts = Post.objects.filter(status=1)
-#     context = {'posts': posts}
-#     return render(request, 'blog/test.html', context)  
 
-# def test(request, name):
-    
-#     context = {'name': name}
-#     return render(request, 'blog/test.html', context)
-
-# def test(request, name, family_name, age):
-    
-#     context = {'name': name, 'family_name': family_name, 'age': age}
-#     return render(request, 'blog/test.html', context)
-
-# def test(request, name, family_name, age):
-    
-#     context = {'name': name, 'family_name': family_name, 'age': age}
-#     return render(request, 'blog/test.html', context)
-
-# def test(request, pid):
-    
-#     context = {'pid':pid}
-#     return render(request, 'blog/test.html', context)
-
-# def test(request, pid):
-#     post = Post.objects.get(id= pid)
-    
-#     context = {'post':post}
-#     return render(request, 'blog/test.html', context)
-
-# def test(request, pid):
-#     post = get_object_or_404(Post, pk=pid)
-#     context = {'post':post}
-#     return render(request, 'blog/test.html', context)
+def test(request, pid):
+    post = get_object_or_404(Post, pk=pid)
+    context = {'post':post}
+    return render(request, 'blog/test.html', context)
