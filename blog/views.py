b2ae3f40c877e3dce67ea6429e62c3895fbcfbd3
HamidProg-22 <197288102+HamidProg-22@users.noreply.github.com>
2025-11-14 22:11:23 +0330

    pagination and update view
    added paginator on pages and update view and plog-home.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-
 from blog.models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def blog_view(request, **kwargs):
@@ -9,6 +9,15 @@ def blog_view(request, **kwargs):
         posts = posts.filter(category__name = kwargs['cat_name'])
     if kwargs.get('author_username') != None:
         posts = posts.filter(author__username=kwargs['author_username'])
+    posts = Paginator(posts, 3) # Show 3 posts per page
+    
+    try: 
+        page_number = request.GET.get('page')    
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
@@ -27,3 +36,16 @@ def blog_category(request, cat_name):
     posts = posts.filter(category__name=cat_name)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
+
+def blog_search(request):
+    # print(request.__dict__)
+    posts = Post.objects.filter(status=1)   
+    if request.method == 'GET':
+        # print(request.GET.get('s'))
+        if s :=request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
+           
+        
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+    
