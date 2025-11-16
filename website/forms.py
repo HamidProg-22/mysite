0fc2f230ece731ee6b0e2034293dec59e0691992
HamidProg-22 <197288102+HamidProg-22@users.noreply.github.com>
2025-11-16 04:16:43 +0330

    way 2
    added forms.ModelForm

--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,17 @@
 from django import forms
+from website.models import Contact
 
 class NameForm(forms.Form):
     name = forms.CharField(label='Your name', max_length=100)
     email = forms.EmailField(label='Your email')
     subject = forms.CharField(label='Subject', max_length=200)
     message = forms.CharField(label='Message', widget=forms.Textarea)
+    
+
+class ContactForm(forms.ModelForm):
+    
+    class Meta:
+        model = Contact
+        fields = '__all__'
+        
+    
--- website/urls.py
+++ website/urls.py
@@ -1,16 +1,10 @@
-
-
 from django.urls import path
-# the follow codes is generate by me
-# from website.views import http_test # or from .views import http_test
-# from website.views import json_test
 from website.views import *
+
 
 app_name = 'website'
 
 urlpatterns = [
-    # path('http-test', http_test),
-    # path('json-test', json_test),
     path('', index_view, name= 'index'),
     path('about', about_view, name= 'about'),
     path('contact', contact_view, name= 'contact'),
