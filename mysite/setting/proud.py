e3d8114b570ca04103aae0fd660d4af0de1bf4d1
HamidProg-22 <197288102+HamidProg-22@users.noreply.github.com>
2025-11-29 18:21:54 +0330

    create dev and proudation setting
    added dev.py and prou.py for settings project for published

---
+++ mysite/setting/proud.py
@@ -0,0 +1,36 @@
+from mysite.settings import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-v*dw-q8r+)a6@2i(5961!@_aj&j!-touat4g$9@j+&kats_uy='
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# site framework
+# SITE_ID = 2
+SITE_ID = 3
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
+
+# CSRF_COOKIE_SECURE = True
--- mysite/settings.py
+++ mysite/settings.py
@@ -16,16 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v*dw-q8r+)a6@2i(5961!@_aj&j!-touat4g$9@j+&kats_uy='
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -93,15 +83,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -141,11 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
 
-# site framework
-# SITE_ID = 2
-SITE_ID = 3
+
 
 # robots
 ROBOTS_USE_HOST = True
@@ -173,11 +151,7 @@ SUMMERNOTE_CONFIG = {
     }
 }
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -188,4 +162,3 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
